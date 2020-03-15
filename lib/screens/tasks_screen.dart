680fc2c88d5eb562426806bb39fd4bc1772a933a
Rafael WMS <rafael.wms@msn.com>
2020-03-15 05:41:37 -0300

    Callbacks

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -14,7 +14,7 @@ class TasksScreen extends StatelessWidget {
         onPressed: (){
           showModalBottomSheet(
               context: context,
-              isScrollControlled: true,
+             // isScrollControlled: true,
               builder: (context) => AddTaskScreen()
           );
         },
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
+      title: Text('This is a task.',
+      style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null
       ),
+      ),
+      trailing: TaskCheckBox(
+          isChecked: isChecked,
+          toogleCheckbox: (bool newValue){
+            setState(() {
+              isChecked = newValue;
+            });
+          }),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+
+  TaskCheckBox({this.isChecked, this.toogleCheckbox});
+
+  final isChecked;
+  final Function toogleCheckbox;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: toogleCheckbox,
     );
   }
 }
